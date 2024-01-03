d8884afae69c9cd3b03f79f097b0522cbf8c5aa5
MD.  Shanto <135007992+shanto0011@users.noreply.github.com>
2024-01-04 03:06:13 +0600

    Update global.dart

--- Todo-App-with-getx/lib/global.dart
+++ Todo-App-with-getx/lib/global.dart
@@ -1,12 +1,6 @@
+import 'package:chatty/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:todo_app_with_getx/firebase_options.dart';
-//import 'package:todo_app_with_getx/pages/todo-pages/controller.dart';
-import 'package:todo_app_with_getx/services/storage.dart';
-import 'package:todo_app_with_getx/store/user.dart';
-
-import 'pages/testcontroller.dart';
 
 class Global {
   static Future init() async {
@@ -14,9 +8,5 @@ class Global {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    //await Get.putAsync<StorageService>(() => StorageService().init());
-    //Get.put<UserStore>(UserStore());
-    //await Get.put<TodoController>(TodoController());
-    //await Get.put<>
   }
 }
