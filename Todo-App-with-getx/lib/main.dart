d814a624abbb06921e0aa5919db634203ffdafa5
MD.  Shanto <135007992+shanto0011@users.noreply.github.com>
2024-01-04 03:03:17 +0600

    Add files via upload

--- Todo-App-with-getx/lib/main.dart
+++ Todo-App-with-getx/lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_app_with_getx/global.dart';
+import 'package:todo_app_with_getx/routes/app-route.dart';
+import 'package:todo_app_with_getx/routes/route-name.dart';
 
+Future<void> main() async {
+  await Global.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "TODO-APP-WITH-GETX",
+      initialRoute: RouteName.INITIAL,
+      getPages: AppRoutes.routes,
+    );
+  }
+}
